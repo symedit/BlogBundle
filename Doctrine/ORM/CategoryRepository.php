8c7e4273f7ba8891da8a02b1105a74f70bc3d253
Craig Blanchette <craig.blanchette@gmail.com>
2014-01-13 15:05:45 -0500

    Moved PostRepository / CategoryRepository to BlogBundle

---
+++ Doctrine/ORM/CategoryRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the SymEdit package.
+ *
+ * (c) Craig Blanchette <craig.blanchette@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SymEdit\Bundle\BlogBundle\Doctrine\ORM;
+
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+
+class CategoryRepository extends EntityRepository
+{
+    public function findRoots()
+    {
+        return $this->findBy(array(
+            'parent' => null,
+        ));
+    }
+
+    /*
+     * Creates a blank category with all the roots as the children.
+     */
+    public function findRoot()
+    {
+        $root = $this->createNew();
+        $root->setChildren($this->findRoots());
+
+        return $root;
+    }
+}
